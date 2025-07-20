3dcb492a017d804aee847fb7e90e7666af22f368
Anal Patil <123722815+aanalpatil24@users.noreply.github.com>
2025-07-21 02:43:34 +0530

    sql_queries.sql

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,8 +1,7 @@
--- SQL Retail Sales Analysis Project
+-- Retail Sales Analysis Project
 
-CREATE DATABASE IF NOT EXISTS sql_project_p2;
-USE sql_project_p2;
-
+CREATE DATABASE IF NOT EXISTS Retail_Sales_Analysis;
+USE Retail_Sales_Analysis;
 -- Create TABLE
 DROP TABLE IF EXISTS retail_sales;
 CREATE TABLE retail_sales (
@@ -10,12 +9,12 @@ CREATE TABLE retail_sales (
     sale_date DATE,	 
     sale_time TIME,	
     customer_id	INT,
-    gender	VARCHAR(15),
+    gender	VARCHAR(10),
     age	INT,
-    category VARCHAR(15),	
-    quantity	INT,
+    category VARCHAR(25),	
+    quantity INT,
     price_per_unit FLOAT,	
-    cogs	FLOAT,
+    purchase_cost FLOAT,
     total_sale FLOAT
 );
 
@@ -26,10 +25,6 @@ SELECT * FROM retail_sales LIMIT 10;
 SELECT COUNT(*) FROM retail_sales;
 
 -- Data Cleaning Checks
-SELECT * FROM retail_sales WHERE transaction_id IS NULL;
-SELECT * FROM retail_sales WHERE sale_date IS NULL;
-SELECT * FROM retail_sales WHERE sale_time IS NULL;
-
 SELECT * FROM retail_sales
 WHERE 
     transaction_id IS NULL OR
@@ -38,7 +33,7 @@ WHERE
     gender IS NULL OR
     category IS NULL OR
     quantity IS NULL OR
-    cogs IS NULL OR
+    purchase_cost IS NULL OR
     total_sale IS NULL;
 
 -- Delete Null Records
@@ -50,54 +45,57 @@ WHERE
     gender IS NULL OR
     category IS NULL OR
     quantity IS NULL OR
-    cogs IS NULL OR
+    purchase_cost IS NULL OR
     total_sale IS NULL;
+    
+SET SQL_SAFE_UPDATES=1;
 
 -- Data Exploration
+-- Total Sales
 SELECT COUNT(*) AS total_sale FROM retail_sales;
-
+-- Total Unique Customers
 SELECT COUNT(DISTINCT customer_id) AS unique_customers FROM retail_sales;
-
-SELECT DISTINCT category FROM retail_sales;
+-- Total Unique Categories
+SELECT DISTINCT category, COUNT(DISTINCT category) FROM retail_sales GROUP BY category;
 
 -- Q1: Sales on 2022-11-05
 SELECT * FROM retail_sales
 WHERE sale_date = '2022-11-05';
 
--- Q2: Clothing with quantity >= 4 in Nov 2022
+-- Q2: All transactions with category='Clothing' and quantity >= 5 in November 2022
 SELECT * FROM retail_sales
 WHERE 
     category = 'Clothing' AND
     DATE_FORMAT(sale_date, '%Y-%m') = '2022-11' AND
     quantity >= 4;
-
--- Q3: Total sales per category
+    
+-- Q3: Total sales in each category
 SELECT 
     category,
     SUM(total_sale) AS net_sale,
-    COUNT(*) AS total_orders
+    COUNT(category) AS total_orders
 FROM retail_sales
 GROUP BY category;
 
--- Q4: Average age of Beauty buyers
+-- Q4: Average age of buyers in the category of Beauty
 SELECT ROUND(AVG(age), 2) AS avg_age
 FROM retail_sales
 WHERE category = 'Beauty';
 
--- Q5: Transactions where total_sale > 1000
+-- Q5: Transactions where total_sale is greater than 1000
 SELECT * FROM retail_sales
 WHERE total_sale > 1000;
 
--- Q6: Total transactions per gender per category
+-- Q6: Total transactions made by each gender in each category
 SELECT 
     category,
     gender,
     COUNT(*) AS total_trans
 FROM retail_sales
 GROUP BY category, gender
-ORDER BY category;
+ORDER BY category, gender;
 
--- Q7: Best selling month in each year
+-- Q7: Average sale for each month and the best selling month in each year
 SELECT 
     year,
     month,
@@ -111,9 +109,10 @@ FROM (
     FROM retail_sales
     GROUP BY YEAR(sale_date), MONTH(sale_date)
 ) AS ranked_sales
-WHERE rnk = 1;
+WHERE rnk = 1
+ORDER BY year;
 
--- Q8: Top 5 customers by total sales
+-- Q8: Top 5 customers based on the highest total sales
 SELECT 
     customer_id,
     SUM(total_sale) AS total_sales
@@ -122,21 +121,21 @@ GROUP BY customer_id
 ORDER BY total_sales DESC
 LIMIT 5;
 
--- Q9: Unique customers per category
+-- Q9: Number of Unique customers who purchased from each category
 SELECT 
     category,    
-    COUNT(DISTINCT customer_id) AS cnt_unique_cs
+    COUNT(DISTINCT customer_id) AS unique_customers
 FROM retail_sales
 GROUP BY category;
 
--- Q10: Orders by shift
+-- Q10: Orders by shift (Create each shift and number of orders for example Morning<-12, Afternoon between 12 & 17 etc.) 
 SELECT 
     CASE
         WHEN HOUR(sale_time) < 12 THEN 'Morning'
         WHEN HOUR(sale_time) BETWEEN 12 AND 17 THEN 'Afternoon'
         ELSE 'Evening'
     END AS shift,
-    COUNT(*) AS total_orders
+    COUNT(sale_time) AS total_orders
 FROM retail_sales
 GROUP BY shift;
 
